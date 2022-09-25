8066920fa495d0021e36de7810ab4dcefb9fbb3e
scliang <354767929@qq.com>
2022-09-25 21:20:06 +0800

    add mongodb

---
+++ internal/mongodb/mongo_test.go
@@ -0,0 +1,29 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"github.com/scliang-strive/webServerTools/config"
+	"go.mongodb.org/mongo-driver/bson"
+	"testing"
+)
+
+func TestMongoDb(t *testing.T) {
+	conf := &config.MongoConf{
+		Host:            "xx.xx.xx.xx",
+		User:            "admin",
+		Password:        "admin",
+		MaxPoolSize:     1,
+		MaxConnIdleTime: 5000,
+		ConnectTimeout:  5000,
+	}
+	err := MongoInit(conf)
+	if err != nil {
+		fmt.Println("connect error:", err)
+	}
+	client := GetMongo("admin", "web")
+	fmt.Println("cli", client)
+	filer := bson.D{{"name", "golang"}}
+	bytes, _ := client.FindOne(context.Background(), filer).DecodeBytes()
+	fmt.Println(bytes.String())
+}
---
+++ internal/mongodb/mongodb.go
@@ -0,0 +1,43 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"github.com/scliang-strive/webServerTools/config"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+var MClient *mongo.Client
+
+func MongoInit(cfg *config.MongoConf) (e error) {
+	// 连接uri
+	uri := fmt.Sprintf("mongodb://%s:%s@%s/?connect=direct", cfg.User, cfg.Password, cfg.Host)
+	// 构建mongo连接可选属性配置
+	opt := new(options.ClientOptions)
+	// 设置最大连接的数量
+	opt = opt.SetMaxPoolSize(uint64(cfg.MaxPoolSize))
+	// 设置连接超时时间
+	opt = opt.SetConnectTimeout(time.Duration(cfg.ConnectTimeout) * time.Second)
+	// 设置连接的空闲时间 毫秒
+	opt = opt.SetMaxConnIdleTime(time.Duration(cfg.MaxConnIdleTime))
+	// 开启驱动
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri), opt)
+	if err != nil {
+		return err
+	}
+	// 注意，在这一步才开始正式连接mongo
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	e = client.Ping(ctx, nil)
+	if e != nil {
+		return e
+	}
+	MClient = client
+	return nil
+}
+
+func GetMongo(databaseName, collectionName string) *mongo.Collection {
+	return MClient.Database(databaseName).Collection(collectionName)
+}
